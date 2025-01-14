8d5499c13d7d4431ce89d901fd888e9fed93e95b
WillianRRP <willianrenato754@gmail.com>
2025-01-13 23:01:25 -0300

    config, json e helpers
    O comando para habilitar a gravação de vídeos no Cypress foi adicionado no arquivo de configuração, o comando cypress run foi incluído no package.json, e o teste utilizando o comando its foi inserido no arquivo helpers.

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+     video: true, 
+     videoCompression: 32,
+     videoUploadOnPasses: true, 
+     videosFolder: "cypress/videos",
 
     setupNodeEvents(on, config) {
       
--- cypress/e2e/integration/helpers.cy.js
+++ cypress/e2e/integration/helpers.cy.js
@@ -1,16 +1,16 @@
 /// <reference types="cypress"/>
 
-const { Console } = require("console");
+const { Console } = require('console');
 
-describe("Helpers...", () => {
+describe('Helpers...', () => {
   beforeEach(() => {
-    cy.visit("https://wcaquino.me/cypress/componentes.html");
+    cy.visit('https://wcaquino.me/cypress/componentes.html');
   });
 
-  it("Wrap", () => {
-    const obj = { nome: "User", idade: 20 };
-    expect(obj).to.have.property("nome");
-    cy.wrap(obj).should("have.property", "nome");
+  it('Wrap', () => {
+    const obj = { nome: 'User', idade: 20 };
+    expect(obj).to.have.property('nome');
+    cy.wrap(obj).should('have.property', 'nome');
 
     //cy.get('#formNome').then($el =>{
     //   $el.val('Está funcionando via jquery?')
@@ -24,12 +24,31 @@ describe("Helpers...", () => {
         500;
     });
 
-    cy.get("#buttonSimple").then(() =>
-      console.log("Encontrei o primeiro botão")
+    cy.get('#buttonSimple').then(() =>
+      console.log('Encontrei o primeiro botão')
     );
     //promise.then(num => console.log(num))
 
     cy.wrap(promise).then((ret) => console.log(ret));
-    cy.get("#buttonList").then(() => console.log("Encontrei o segundo botão"));
+    cy.get('#buttonList').then(() => console.log('Encontrei o segundo botão'));
   });
+
+
+  it('Its...', () => {
+    const obj = { nome: 'User', idade: 20 };
+    cy.wrap(obj).should('have.property', 'nome', 'User');
+    cy.wrap(obj).its('nome').should('be.equal', 'User');
+
+    const obj2 = {nome: "user", idade: 20, endereco: { rua: 'dos cachorros'} }
+    cy.wrap(obj2).its('endereco').should('have.property', 'rua');
+    cy.wrap(obj2).its('endereco').its('rua').should('contain', 'cachorros');
+    cy.wrap(obj2).its('endereco.rua').should('contain', 'cachorros');
+
+
+    cy.visit('https://wcaquino.me/cypress/componentes.html');
+    cy.title().its('length').should('be.equal', 20)
+
+
+
+  })
 });
