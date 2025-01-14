e933f4676054a8cf3c2c8c66e06ff6bc3e45d77b
WillianRRP <willianrenato754@gmail.com>
2025-01-13 23:04:35 -0300

    arrumando

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
      video: true, 
      videoCompression: 32,
-     videoUploadOnPasses: true, 
+     videoUploadOnPasses: false, 
      videosFolder: "cypress/videos",
 
     setupNodeEvents(on, config) {
