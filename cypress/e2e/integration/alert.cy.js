04367b87bc1c09d2858a5cf62f4c172b113c2832
WillianRRP <willianrenato754@gmail.com>
2025-01-20 22:24:40 -0300

    alterações na sintaxe do alert e criação do popup

--- cypress/e2e/integration/alert.cy.js
+++ cypress/e2e/integration/alert.cy.js
@@ -62,21 +62,29 @@ describe("Work with alerts", () => {
   it.only("Validando mensagens", () => {
     const stub = cy.stub().as("alerta");
     cy.on("window:alert", stub);
-    cy.get('#formCadastrar').click()
-     .then(() => expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+    cy.get("#formCadastrar")
+      .click()
+      .then(() =>
+        expect(stub.getCall(0)).to.be.calledWith("Nome eh obrigatorio")
+      );
 
-        cy.get('#formNome').type('Roger')
-        cy.get('#formCadastrar').click()
-        .then(() => expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
-        cy.get('[data-cy="dataSobrenome"]').type('Da silveira')
+    cy.get("#formNome").type("Roger");
+    cy.get("#formCadastrar")
+      .click()
+      .then(() =>
+        expect(stub.getCall(1)).to.be.calledWith("Sobrenome eh obrigatorio")
+      );
+    cy.get('[data-cy="dataSobrenome"]').type("Da silveira");
 
-        cy.get('#formCadastrar').click()
-        .then(() => expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio'))
+    cy.get("#formCadastrar")
+      .click()
+      .then(() =>
+        expect(stub.getCall(2)).to.be.calledWith("Sexo eh obrigatorio")
+      );
 
-        cy.get('#formSexoFem').click()
-        cy.get('#formCadastrar').click()
+    cy.get("#formSexoFem").click();
+    cy.get("#formCadastrar").click();
 
-        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
-      
-  })
+    cy.get("#resultado > :nth-child(1)").should("contain", "Cadastrado!");
+  });
 });
