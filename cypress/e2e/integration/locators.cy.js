53fc5fb517ad324bd0662414becd561f2d7eaeec
WillianRRP <willianrenato754@gmail.com>
2025-01-24 19:40:01 -0300

    .

---
+++ cypress/e2e/integration/locators.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+describe("Work with alerts", () => {
+  beforeEach(() => {
+    cy.visit("https://wcaquino.me/cypress/componentes.html");
+  });
+
+  beforeEach(() => {
+    cy.reload();
+  });
+  it("...", () => {
+   
+    });
+  
+})  
