8392f93d901312c2332d2b6ec0998ac4f9010ee6
WillianRRP <willianrenato754@gmail.com>
2025-01-24 21:56:50 -0300

    locators e xpath

--- cypress/e2e/integration/locators.cy.js
+++ cypress/e2e/integration/locators.cy.js
@@ -7,8 +7,17 @@ describe("Work with alerts", () => {
   beforeEach(() => {
     cy.reload();
   });
-  it("...", () => {
-   
-    });
-  
-})  
+  it("Using jquery selector", () => {
+    cy.get(':nth-child(5) > :nth-child(6) > [type="text"]');
+    cy.get("[onclick*='Usuario B']");
+    cy.get("#tabelaUsuarios td:contains('Doutorado'):eq(0) ~ td:eq(3) > input");
+    cy.get("#tabelaUsuarios td:contains('Doutorado'):eq(2) ~ td:eq(3) >input");
+    cy.get("#tabelaUsuarios tr:contains('Doutorado'):eq(1) td:eq(6) input");
+  });
+  it.only("using xpath", () => {
+    cy.xpath("//input[contains(@onclick, 'Usuario B')]");
+    cy.xpath("//table[@id='tabelaUsuarios']//td[contains(., 'Usuario B')]/..//input[@type='text']");
+    cy.xpath("//td[contains(., 'Usuario A')]/following-sibling::td[contains(., 'Mestrado')]/..//input[@type='text']")
+
+  })
+});
