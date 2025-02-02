8af20c37843927c346973486db729adbf4a65ce8
WillianRRP <willianrenato754@gmail.com>
2025-02-01 21:31:52 -0300

    add teste e arrumando umas coisas

---
+++ cypress/e2e/integration/teste real/funcional.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress"/>
+import loc from "../../../support/locators";
+import "../../../support/commandsContas";
+
+describe("Should test at a funcional level", () => {
+  beforeEach(() => {
+    cy.login("a@a", "a");
+    cy.resetApp()
+  });
+
+  it("should create account", () => {
+    cy.acessaMenuConta();
+    cy.InserirConta("conta de teste");
+    cy.get(loc.MENSSAGE).should("contain", "Conta inserida com sucesso");
+  });
+  it("should update an account", () => {
+    cy.acessaMenuConta();
+    cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR("Conta para alterar")).click();
+    cy.get(loc.CONTAS.NOME).clear().type("Conta alterada");
+    cy.get(loc.CONTAS.BTN_SALVAR).click();
+    cy.get(loc.MENSSAGE).should("contain", "Conta atualizada com sucesso");
+  });
+});
