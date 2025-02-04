93caa1cc77cc96a697e18b59e4c150335e594d0c
WillianRRP <willianrenato754@gmail.com>
2025-02-04 17:23:51 -0300

    criando o getToken

--- cypress/e2e/integration/teste real/back.cy.js
+++ cypress/e2e/integration/teste real/back.cy.js
@@ -1,39 +1,38 @@
 /// <reference types='cypress'/>
 
 describe('Should test at a funcional level', () => {
+  let token
+  
   beforeEach(() => {
     //cy.login('a@a', 'a');
     //cy.resetApp()
     //cy.get(loc.MENU.HOME).click()
+    cy.getToken('testewp@a', 'a')
+    .then(tkn => {
+      token = tkn 
+
+    })
+    
   });
 
+ //para este teste funfar a base deve ser resetada após o teste ser rodado
   it('should create account', () => {
-   cy.request({
-     method: 'POST',
-     url: 'https://barrigarest.wcaquino.me/signin',
-     body:{
-       email: "testewp@a",
-       redirecionar: false,
-       senha: "a"
-     }
-   }).its('body.token').should('not.be.empty')
-   .then(token => {
+   cy.getToken('a@a', 'a')
     cy.request({
       method: 'POST',
       headers: { Authorization: `JWT ${token}`},
       url: 'https://barrigarest.wcaquino.me/contas',
       body:{
-        nome: 'Conta de teste via rest 22'
+        nome: 'Conta de teste via rest 12'
   
       }
     }).as("resonse")
-   })
 
 
    cy.get('@resonse').then(res =>{
     expect(res.status).to.be.equal(201)
     expect(res.body).to.have.property('id')
-    expect(res.body).to.have.property('nome', 'Conta de teste via rest 22')
+    expect(res.body).to.have.property('nome', 'Conta de teste via rest 12')
    })
 
    
