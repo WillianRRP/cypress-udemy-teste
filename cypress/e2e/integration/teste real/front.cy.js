11d9df1f52dec61ce3a4f0ba30525e9a43f8b140
WillianRRP <willianrenato754@gmail.com>
2025-02-14 19:44:14 -0300

    teste para concluir

--- cypress/e2e/integration/teste real/front.cy.js
+++ cypress/e2e/integration/teste real/front.cy.js
@@ -192,7 +192,7 @@ cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso')
     cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao')).click()
     cy.get(loc.MESSAGE).should('contain', 'sucesso')
   });
-  it.only('should validate to date send create account', () => {
+  it('should validate to date send create account', () => {
 
     const reqStub = cy.stub()
     cy.intercept({
@@ -227,4 +227,20 @@ cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso')
     cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
 })
 
+
+//todo
+it.only('Should test colors', () => {
+  cy.intercept({
+    method: 'GET',
+    url: '/extrato/**'
+},
+[
+  { "conta": "Conta para movimentacoes", "id": 31434, "descricao": "Receita paga", "envolvido": "AAA", "observacao": null, "tipo": "REC", "data_transacao": "2019-11-13T03:00:00.000Z", "data_pagamento": "2019-11-13T03:00:00.000Z", "valor": "-1500.00", "status": true, "conta_id": 42077, "usuario_id": 1, "transferencia_id": null, "parcelamento_id": null },
+  { "conta": "Conta com movimentacao", "id": 31435, "descricao": "Receita pendente", "envolvido": "BBB", "observacao": null, "tipo": "REC", "data_transacao": "2019-11-13T03:00:00.000Z", "data_pagamento": "2019-11-13T03:00:00.000Z", "valor": "-1500.00", "status": false, "conta_id": 42078, "usuario_id": 1, "transferencia_id": null, "parcelamento_id": null },
+  { "conta": "Conta para saldo", "id": 31436, "descricao": "Despesa paga", "envolvido": "CCC", "observacao": null, "tipo": "REC", "data_transacao": "2019-11-13T03:00:00.000Z", "data_pagamento": "2019-11-13T03:00:00.000Z", "valor": "3500.00", "status": true, "conta_id": 42079, "usuario_id": 1, "transferencia_id": null, "parcelamento_id": null },
+  { "conta": "Conta para saldo", "id": 31437, "descricao": "Despesa pendente", "envolvido": "DDD", "observacao": null, "tipo": "DESP", "data_transacao": "2019-11-13T03:00:00.000Z", "data_pagamento": "2019-11-13T03:00:00.000Z", "valor": "-1000.00", "status": false, "conta_id": 42079, "usuario_id": 1, "transferencia_id": null, "parcelamento_id": null },
+])
+
+  cy.get(loc.MENU.EXTRATO).click()
+})
 });
